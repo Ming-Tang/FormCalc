55aff307d5e0c79284b5194528a3c3900e0132df
Ming <project.shinkirou@gmail.com>
2014-03-18 13:10:49 -0700

    added js/cc

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+NODE=node
+JSCC_NODE_PATH=./node_modules/jscc-node/
+DRIVER=$JSCC_NODE_PATH/driver_node.js_
+PARSER_INPUT=math.par
+PARSER_OUTPUT=math_parser.js
+
+$NODE $JSCC_NODE_PATH/jscc.js -t $DRIVER -o $PARSER_OUTPUT $PARSER_INPUT
