1a30208cfc840c98b8c3ab41995b046df4e08432
Ming <project.shinkirou@gmail.com>
2014-03-18 13:32:36 -0700

    added driver

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 NODE=node
 JSCC_NODE_PATH=./node_modules/jscc-node/
-DRIVER=$JSCC_NODE_PATH/driver_node.js_
+DRIVER=$JSCC_NODE_PATH/driver.js_
 PARSER_INPUT=math.par
 PARSER_OUTPUT=math_parser.js
 
